b9079179d3bcc8b60b89419ddfa1f043f463d475
Miguel Pais <miguel.pais@sysdig.com>
2023-03-21 14:48:56 +0100

    Implementing a stack

---
+++ stack/src/main.rs
@@ -0,0 +1,14 @@
+pub mod stack;
+
+use stack::Stack;
+
+fn main() {
+    let mut stack = Stack::from(3).push(3).push(4).push(5);
+    stack.pop();
+    stack.pop();
+    stack.pop();
+    stack.pop();
+    stack.pop();
+    stack.pop();
+    println!("{:?}", stack);
+}
---
+++ stack/src/stack.rs
@@ -0,0 +1,67 @@
+#[derive(Debug, Clone)]
+pub struct Stack<T: Clone> {
+    top: Option<StackElement<T>>,
+    size: u32
+}
+
+#[derive(Debug, Clone)]
+struct StackElement<T: Clone> {
+    value: T,
+    next: Option<Box<StackElement<T>>>
+}
+
+impl<T: Clone> Stack<T> {
+    pub fn empty() -> Stack<T> {
+        Stack {
+            top: None,
+            size: 0
+        }
+    }
+
+    pub fn from(value: T) -> Stack<T> {
+        Stack {
+            top: Some(StackElement {
+                value,
+                next: None
+            }),
+            size: 1
+        }
+    }
+
+    pub fn push(mut self, value: T) -> Stack<T> {
+        if self.size == 0 {
+            self.top = Some(StackElement {
+                value,
+                next: None
+            })
+        }
+        else {
+            let old_top = self.top.clone().unwrap();
+            self.top = Some(StackElement {
+                value,
+                next: Some(Box::new(old_top))
+            })
+        }
+        self.size += 1;
+        self
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        if self.size <= 0 {
+            return None;
+        }
+        else if self.size == 1 {
+            let old_top = self.top.clone().unwrap();
+            self.top = None;
+            self.size -= 1;
+            return Some(old_top.value);
+        }
+        else {
+            let old_top = self.top.clone().unwrap();
+            let next_top = *old_top.next.clone().unwrap();
+            self.top = Some(next_top);
+            self.size -= 1;
+            return Some(old_top.value);
+        }
+    }
+}
